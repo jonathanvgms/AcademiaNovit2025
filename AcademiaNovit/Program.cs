34f8cbbd269a694b7b46503e74fb3cb8f702f3a9
jvelazquez <jonathanvgms@gmail.com>
2025-05-27 20:35:06 -0300

    Add dockerfile

--- AcademiaNovit/Program.cs
+++ AcademiaNovit/Program.cs
@@ -12,11 +12,8 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.MapControllers();
 
